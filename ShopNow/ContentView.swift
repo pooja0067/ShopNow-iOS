c6ef12696349962d3db6beb9b71f39b74725a9f1
ganesh <pp0004200@gmail.com>
2025-12-08 11:21:03 +0530

    shopnow ios app

---
+++ ShopNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopNow
+//
+//  Created by Pooja on 08/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShopNow/ShopNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopNowApp.swift
+//  ShopNow
+//
+//  Created by Pooja on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
